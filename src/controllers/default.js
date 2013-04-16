a2786dcac6bee99bab4d6ac7e0d6f55c80067899
Kyle Penniston <krp530@truman.edu>
2013-04-16 17:44:55 -0500

    made everything draggable.

--- src/controllers/default.js
+++ src/controllers/default.js
@@ -1,21 +1,63 @@
-      var container = document.getElementById("container");
-      var stage = new Kinetic.Stage({
-        container: 'container',
-        width: container.clientWidth,
-        height: container.clientHeight,
-        draggable: true
-      });
-      var layer = new Kinetic.Layer();
+var container=document.getElementById("container");
+var stage = new Kinetic.Stage({
+  container: 'container',
+  width: container.clientWidth,
+  height: container.clientHeight,
+  draggable: true
+});
+var layer = new Kinetic.Layer();
 
-      var contention = new Kinetic.Shape({
-        drawFunc: function(canvas) {
+var contention = new Kinetic.Shape({
+  drawFunc: function(canvas) {
+    var context = canvas.getContext();
+    context.beginPath();
+    x=container.clientWidth/2;
+    y=container.clientHeight/2;
+    w=200;
+    h=100;
+    r=11;
+    context.moveTo(x-0.5*w+r, y-0.5*h);
+    context.arcTo(x+0.5*w, y-0.5*h, x+0.5*w, y+0.5*h, r);
+    context.arcTo(x+0.5*w, y+0.5*h, x-0.5*w, y+0.5*h, r);
+    context.arcTo(x-0.5*w, y+0.5*h, x-0.5*w, y-0.5*h, r);
+    context.arcTo(x-0.5*w, y-0.5*h, x+0.5*w, y-0.5*h, r);
+    context.closePath();
+    canvas.fillStroke(this);
+  },
+  fill: 'white',
+  stroke: 'black',
+  draggable: true,
+  strokeWidth: 4
+});
+var supportButton = new Kinetic.Shape({
+  drawFunc: function(canvas) {
+    var context = canvas.getContext();
+    context.beginPath();
+    x=container.clientWidth/2;
+    y=container.clientHeight/2;
+    w=50;
+    h=50;
+    r=5;
+    context.moveTo(x-0.5*w+r, y-0.5*h);
+    context.arcTo(x+0.5*w, y-0.5*h, x+0.5*w, y+0.5*h, r);
+    context.arcTo(x+0.5*w, y+0.5*h, x-0.5*w, y+0.5*h, r);
+    context.arcTo(x-0.5*w, y+0.5*h, x-0.5*w, y-0.5*h, r);
+    context.arcTo(x-0.5*w, y-0.5*h, x+0.5*w, y-0.5*h, r);
+    context.closePath();
+    canvas.fillStroke(this);
+  },
+  draggable: true,
+  fill: 'green',
+  stroke: 'black',
+  strokeWidth: 2,
+        drawHitFunc: function(canvas) {
           var context = canvas.getContext();
           context.beginPath();
           x=container.clientWidth/2;
           y=container.clientHeight/2;
-          w=200;
-          h=100;
-          r=11;
+          w=50;
+          h=50;
+          r=5;
           context.moveTo(x-0.5*w+r, y-0.5*h);
           context.arcTo(x+0.5*w, y-0.5*h, x+0.5*w, y+0.5*h, r);
           context.arcTo(x+0.5*w, y+0.5*h, x-0.5*w, y+0.5*h, r);
@@ -23,14 +65,20 @@
           context.arcTo(x-0.5*w, y-0.5*h, x+0.5*w, y-0.5*h, r);
           context.closePath();
           canvas.fillStroke(this);
-        },
-        fill: 'white',
-        stroke: 'black',
-        strokeWidth: 4
-      });
+        }
+});
 
-      // add the contention to the layer
-      layer.add(contention);
+supportButton.on('mousedown', function() {
+        alert('Mousedown supportButton');
+});
 
-      // add the layer to the stage
-      stage.add(layer);
+// add the contention to the layer
+layer.add(contention);
+layer.add(supportButton);
+
+// add the layer to the stage
+stage.add(layer);
+
+if (typeof(supportButton)!=='undefined'){
+  alert('support button exists');
+};
