97edb463cbdf6db72257836158a34457fe438af2
Kyle Penniston <krp530@truman.edu>
2013-04-18 13:14:39 -0500

    added more test code

--- src/controllers/default.js
+++ src/controllers/default.js
@@ -1,83 +1,64 @@
-var container=document.getElementById("container");
+var container = document.getElementById("container");
 var stage = new Kinetic.Stage({
-  container: 'container',
-  width: container.clientWidth,
-  height: container.clientHeight,
-  draggable: true
+	container : 'container',
+	width : container.clientWidth,
+	height : container.clientHeight,
+	draggable : true
 });
 var layer = new Kinetic.Layer();
 
 var contention = new Kinetic.Shape({
-  drawFunc: function(canvas) {
-    var context = canvas.getContext();
-    context.beginPath();
-    x=container.clientWidth/2;
-    y=container.clientHeight/2;
-    w=200;
-    h=100;
-    r=11;
-    context.moveTo(x-0.5*w+r, y-0.5*h);
-    context.arcTo(x+0.5*w, y-0.5*h, x+0.5*w, y+0.5*h, r);
-    context.arcTo(x+0.5*w, y+0.5*h, x-0.5*w, y+0.5*h, r);
-    context.arcTo(x-0.5*w, y+0.5*h, x-0.5*w, y-0.5*h, r);
-    context.arcTo(x-0.5*w, y-0.5*h, x+0.5*w, y-0.5*h, r);
-    context.closePath();
-    canvas.fillStroke(this);
-  },
-  fill: 'white',
-  stroke: 'black',
-  draggable: true,
-  strokeWidth: 4
-});
-var supportButton = new Kinetic.Shape({
-  drawFunc: function(canvas) {
-    var context = canvas.getContext();
-    context.beginPath();
-    x=container.clientWidth/2;
-    y=container.clientHeight/2;
-    w=50;
-    h=50;
-    r=5;
-    context.moveTo(x-0.5*w+r, y-0.5*h);
-    context.arcTo(x+0.5*w, y-0.5*h, x+0.5*w, y+0.5*h, r);
-    context.arcTo(x+0.5*w, y+0.5*h, x-0.5*w, y+0.5*h, r);
-    context.arcTo(x-0.5*w, y+0.5*h, x-0.5*w, y-0.5*h, r);
-    context.arcTo(x-0.5*w, y-0.5*h, x+0.5*w, y-0.5*h, r);
-    context.closePath();
-    canvas.fillStroke(this);
-  },
-  fill: 'green',
-  stroke: 'black',
-  strokeWidth: 2,
-        drawHitFunc: function(canvas) {
-          var context = canvas.getContext();
-          context.beginPath();
-          x=container.clientWidth/2;
-          y=container.clientHeight/2;
-          w=50;
-          h=50;
-          r=5;
-          context.moveTo(x-0.5*w+r, y-0.5*h);
-          context.arcTo(x+0.5*w, y-0.5*h, x+0.5*w, y+0.5*h, r);
-          context.arcTo(x+0.5*w, y+0.5*h, x-0.5*w, y+0.5*h, r);
-          context.arcTo(x-0.5*w, y+0.5*h, x-0.5*w, y-0.5*h, r);
-          context.arcTo(x-0.5*w, y-0.5*h, x+0.5*w, y-0.5*h, r);
-          context.closePath();
-          canvas.fillStroke(this);
-        }
+	drawFunc : function(canvas) {
+		var context = canvas.getContext();
+		context.beginPath();
+		x = container.clientWidth / 2;
+		y = container.clientHeight / 2;
+		w = 200;
+		h = 100;
+		r = 11;
+		context.moveTo(x - 0.5 * w + r, y - 0.5 * h);
+		context.arcTo(x + 0.5 * w, y - 0.5 * h, x + 0.5 * w, y + 0.5 * h, r);
+		context.arcTo(x + 0.5 * w, y + 0.5 * h, x - 0.5 * w, y + 0.5 * h, r);
+		context.arcTo(x - 0.5 * w, y + 0.5 * h, x - 0.5 * w, y - 0.5 * h, r);
+		context.arcTo(x - 0.5 * w, y - 0.5 * h, x + 0.5 * w, y - 0.5 * h, r);
+		context.closePath();
+		canvas.fillStroke(this);
+	},
+	fill : 'white',
+	stroke : 'black',
+	draggable : true,
+	strokeWidth : 4,
+	drawHitFunc : function(canvas) {
+		var context = canvas.getContext();
+		context.beginPath();
+		x = container.clientWidth / 2;
+		y = container.clientHeight / 2;
+		w = 200;
+		h = 100;
+		r = 11;
+		context.moveTo(x - 0.5 * w + r, y - 0.5 * h);
+		context.arcTo(x + 0.5 * w, y - 0.5 * h, x + 0.5 * w, y + 0.5 * h, r);
+		context.arcTo(x + 0.5 * w, y + 0.5 * h, x - 0.5 * w, y + 0.5 * h, r);
+		context.arcTo(x - 0.5 * w, y + 0.5 * h, x - 0.5 * w, y - 0.5 * h, r);
+		context.arcTo(x - 0.5 * w, y - 0.5 * h, x + 0.5 * w, y - 0.5 * h, r);
+		context.closePath();
+		canvas.fillStroke(this);
+	}
 });
 
-supportButton.on('mousedown', function() {
-        alert('Mousedown supportButton');
+var testClaim = Object.create(claim);
+var testCounter = Object.create(counter);
+testClaim.setId(testCounter);
+claimController.setClaimText(testClaim,'foooooooobar');
+
+contention.on('mouseup', function() {
+	div = document.getElementById('myTextArea');
+	div.innerHTML=canvasController.makeTextArea(testClaim);
+	document.getElementById('working').focus();
 });
 
 // add the contention to the layer
 layer.add(contention);
-layer.add(supportButton);
 
 // add the layer to the stage
-stage.add(layer);
-
-if (typeof(supportButton)!=='undefined'){
-  alert('support button exists');
-};
+stage.add(layer); 
