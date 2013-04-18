c6da08ddded8b437abd563faedb66d1a0583a422
Kyle Penniston <krp530@truman.edu>
2013-04-18 13:13:03 -0500

    Modified canvas files to include textarea functionality.

--- src/controllers/canvasController.js
+++ src/controllers/canvasController.js
@@ -1,5 +1,23 @@
 var canvasController = {
 	makeTextArea: function(claim){
-		return "<textarea rows='3' cols='20'>" + claimController.getClaimText(claim) + "</textarea>"
+		document.getElementById('myTextArea').style.left='590px';
+		document.getElementById('myTextArea').style.top='290px';
+		return "<textarea rows='3' cols='20' id='working' onfocus='this.select();' onblur='canvasController.removeTextArea(claim)'>" + claimController.getClaimText(claim) + "</textarea>"
+	},
+	extractText: function(){
+		return document.getElementById('working').innerHTML;
+	},
+	removeTextArea: function(claim){
+		claimController.setClaimText(claim,this.extractText());
+		document.getElementById('myTextArea').innerHTML='';
+	},
+	addClaim: function(){	//Fires when addSupportButton or addRefuteButton are clicked
+		
+	},
+	removeClaim: function(){	//Fires when closeButton of a claim are clicked
+		
+	},
+	clearCanvas: function(){	//Fires when closeButton of contention is clicked
+		
 	}
 };
--- src/tests/TestCode/canvasControllerTest.js
+++ src/tests/TestCode/canvasControllerTest.js
@@ -1,8 +1,17 @@
+var testCounter = Object.create(counter);
 var testClaim = Object.create(claim);
+testClaim.setId(testCounter);
 var testClaimController = Object.create(claimController);
-var testCanvas = Object.create(canvas);
+var testCanvasController = Object.create(canvasController);
 	
 test('makeTextArea()', function() { 
 	var text=claimController.getClaimText(testClaim);
-    ok(testCanvas.makeTextArea(testClaim)==="<textarea rows='3' cols='20'>" + text + "</textarea>", "Successfully made textarea");
+    ok(testCanvasController.makeTextArea(testClaim)==="<textarea rows='3' cols='20' id='working' onfocus='this.select();' onblur='canvasController.removeTextArea(claim)'>" + text + "</textarea>", "Successfully made textarea");
+})
+
+test('extractTest()', function() {
+	var div = document.getElementById('testing');
+	testClaimController.setClaimText(testClaim,'foooooooooooooo... *splat*');
+	div.innerHTML=testCanvasController.makeTextArea(testClaim);
+	ok(testCanvasController.extractText()===testClaimController.getClaimText(testClaim));
 })
