e173e062f9b7a881bd65f42c990e8706db924ea2
Kyle Penniston <krp530@truman.edu>
2013-04-19 23:36:09 -0500

    modified functions to work with localStorage objects.

--- src/controllers/canvasController.js
+++ src/controllers/canvasController.js
@@ -1,15 +1,22 @@
 var canvasController = {
-	makeTextArea: function(claim){
-		document.getElementById('myTextArea').style.left='590px';
-		document.getElementById('myTextArea').style.top='290px';
-		return "<textarea rows='3' cols='20' id='working' onfocus='this.select();' onblur='canvasController.removeTextArea(claim)'>" + claimController.getClaimText(claim) + "</textarea>"
+	makeTextArea: function(myId, shapeX, shapeY){
+		var myClaim = store.get(myId);
+		document.getElementById('myTextArea').style.left=shapeX+7 + "px";
+		document.getElementById('myTextArea').style.top=shapeY+15 + "px";
+    document.getElementById('myTextArea').style.width=300 + "px";
+    document.getElementById('myTextArea').style.height=200 + "px";
+		return "<textarea rows='3' cols='20' id='working' onfocus='this.select();' onblur='canvasController.removeTextArea("+myId+")'>" + claimController.getClaimText(myId) + "</textarea>"
 	},
 	extractText: function(){
-		return document.getElementById('working').innerHTML;
+		return document.getElementById('working').value;
 	},
-	removeTextArea: function(claim){
-		claimController.setClaimText(claim,this.extractText());
+	removeTextArea: function(myId){
+		claimController.setClaimText(myId,this.extractText());
 		document.getElementById('myTextArea').innerHTML='';
+		//document.getElementById('myTextArea').style.left=0 + "px";
+    //document.getElementById('myTextArea').style.top=0 + "px";
+    document.getElementById('myTextArea').style.width=0 + "px";
+    document.getElementById('myTextArea').style.height=0 + "px";
 	},
 	addClaim: function(){	//Fires when addSupportButton or addRefuteButton are clicked
 		
--- src/controllers/claimController.js
+++ src/controllers/claimController.js
@@ -1,11 +1,18 @@
 var claimController = {
-	sendClick: function(){
-		return 1;
+	makeNewClaim: function(parent, newType){
+		var myParent = store.get(parent);
+		var myId = localStorage.length;
+		var myX = parseInt(myParent.x)+100;
+		var myY = parseInt(myParent.y)+100;
+		store.set(myId, {id: myId, type: newType, text: "", parent: myParent.id, x: myX,y: myY})
+		return myId;
 	},
-	getClaimText: function(claim){
-		return claim.getText();
+	getClaimText: function(id){
+		return store.get(id).text;
 	},
-	setClaimText: function(claim, text){
-		claim.setText(text);
+	setClaimText: function(myId, newText){
+		var claim = store.get(myId);
+		claim.text=newText;
+		store.set(myId, claim);
 	}
 };
