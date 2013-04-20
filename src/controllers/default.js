96f9c37b801dc10b7a851f35a748259568c5f4d2
Kyle Penniston <krp530@truman.edu>
2013-04-19 23:38:58 -0500

    transferred and updated good code from default.js to index.html

--- src/controllers/default.js
+++ src/controllers/default.js
@@ -11,8 +11,8 @@ var contention = new Kinetic.Shape({
 	drawFunc : function(canvas) {
 		var context = canvas.getContext();
 		context.beginPath();
-		x = container.clientWidth / 2;
-		y = container.clientHeight / 2;
+		x = parseInt(claim.x);
+		y = parseInt(claim.y);
 		w = 200;
 		h = 100;
 		r = 11;
@@ -31,10 +31,10 @@ var contention = new Kinetic.Shape({
 	drawHitFunc : function(canvas) {
 		var context = canvas.getContext();
 		context.beginPath();
-		x = container.clientWidth / 2;
-		y = container.clientHeight / 2;
-		w = 200;
-		h = 100;
+		x = parseInt(claim.x) - 100;
+		y = parseInt(claim.y) - 50;
+		w = 150;
+		h = 75;
 		r = 11;
 		context.moveTo(x - 0.5 * w + r, y - 0.5 * h);
 		context.arcTo(x + 0.5 * w, y - 0.5 * h, x + 0.5 * w, y + 0.5 * h, r);
@@ -46,19 +46,31 @@ var contention = new Kinetic.Shape({
 	}
 });
 
-var testClaim = Object.create(claim);
-var testCounter = Object.create(counter);
-testClaim.setId(testCounter);
-claimController.setClaimText(testClaim,'foooooooobar');
+//var testClaim = Object.create(claim);
+//var testCounter = Object.create(counter);
+//testClaim.setId(testCounter);
+//claimController.setClaimText(testClaim,'foooooooobar');
+localStorage.clear();
+store.set(0, {id: 0, type: 'support', text: "", parent: 900, x: 500,y: 100});
+var testId = claimController.makeNewClaim(0, 'refutation');
+var text=claimController.getClaimText(testId);
+var claim = store.get(testId);
 
-contention.on('mouseup', function() {
-	div = document.getElementById('myTextArea');
-	div.innerHTML=canvasController.makeTextArea(testClaim);
+layer.on('click', function(event) {
+	alert(event.targetNode.getAbsolutePosition().x+","+event.targetNode.getAbsolutePosition().y);
+	var node = event.targetNode;
+	var div = document.getElementById('myTextArea');
+	div.innerHTML=canvasController.makeTextArea(testId);
 	document.getElementById('working').focus();
+	//document.getElementById('working').textContent = "changed text";
+	//document.getElementById('working').innerText = "changed text";
 });
 
 // add the contention to the layer
 layer.add(contention);
-
 // add the layer to the stage
 stage.add(layer); 
+alert(contention.getAbsolutePosition().x+","+contention.getAbsolutePosition().y);
+/*for (var i = 0; i < localStorage.length; i++){	//iterate through every index of localStorage
+    localStorage.setItem(localStorage.key(i),null);
+}*/
