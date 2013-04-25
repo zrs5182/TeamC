092ee684a38f87e380748cc3d2d88da9a4600bc7
Anup Shrestha <anup133@gmail.com>
2013-04-25 11:30:59 -0500

    Existence of button test

---
+++ src/tests/TestCode/testButton.js
@@ -0,0 +1,16 @@
+
+test('SupportButtonExist', function(){
+	 ok((supportButton) !== null, "Support Button Exist");
+	
+ });
+
+ test('RefuteButtonExist', function(){
+	 ok((refuteButton)!== null, "Refute Button Exist");
+	
+ });
+
+ test('CloseButtonExist', function(){
+	 ok((closeButton)!== null, "Close Button Exist");
+	
+});
+
