2137f6621949bc675057f64684561b5c44157839
Kyle Penniston <krp530@truman.edu>
2013-04-25 03:30:49 -0500

    new variable names and textarea dimensions

--- src/tests/TestCode/canvasControllerTest.js
+++ src/tests/TestCode/canvasControllerTest.js
@@ -1,9 +1,9 @@
 test('makeTextArea()', function() { 
 	localStorage.clear();
 	store.set(0, {id: 0, type: 'support', text: "", parent: 900, x: 300,y: 300});
-	var testId = claimController.makeNewClaim(0, 'refutation');
-	var text=claimController.getClaimText(testId);
-    ok(canvasController.makeTextArea(testId)==="<textarea rows='3' cols='20' id='working' onfocus='this.select();' onblur='canvasController.removeTextArea(someId)'>" + text + "</textarea>", "Successfully made textarea");
+	var myId = claimController.makeNewClaim(0, 'refutation');
+	var text=claimController.getClaimText(myId);
+    ok(canvasController.makeTextArea(myId)==="<textarea rows='7' cols='25' id='working' onfocus='this.select();' onblur='canvasController.removeTextArea(" + myId + ")'>" + claimController.getClaimText(myId) + "</textarea>", "Successfully made textarea");
 })
 
 test('extractText()', function() {
