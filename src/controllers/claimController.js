3cd94de1b386340e548365f63cd61a7a3d05d706
Kyle Penniston <krp530@truman.edu>
2013-04-25 03:31:10 -0500

    new copy

--- src/controllers/claimController.js
+++ src/controllers/claimController.js
@@ -1,18 +1,25 @@
 var claimController = {
-	makeNewClaim: function(parent, newType){
-		var myParent = store.get(parent);
-		var myId = localStorage.length;
-		var myX = parseInt(myParent.x)+100;
-		var myY = parseInt(myParent.y)+100;
-		store.set(myId, {id: myId, type: newType, text: "", parent: myParent.id, x: myX,y: myY})
-		return myId;
-	},
-	getClaimText: function(id){
-		return store.get(id).text;
-	},
-	setClaimText: function(myId, newText){
-		var claim = store.get(myId);
-		claim.text=newText;
-		store.set(myId, claim);
-	}
+  makeNewClaim : function(parent, newType) {
+    var myParent = store.get(parent);
+    var myId = localStorage.length;
+    var myX = parseInt(myParent.x) + 100;
+    var myY = parseInt(myParent.y) + 100;
+    store.set(myId, {
+      id : myId,
+      type : newType,
+      text : "",
+      parent : myParent.id,
+      x : myX,
+      y : myY
+    })
+    return myId;
+  },
+  getClaimText : function(id) {
+    return store.get(id).text;
+  },
+  setClaimText : function(myId, newText) {
+    var claim = store.get(myId);
+    claim.text = newText;
+    store.set(myId, claim);
+  }
 };
